6851e2978ac7f6f17063ce2e059e002b38fe8b6f
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2022-11-04 11:03:26 -0600

    Modificacion del titulo de la pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>🚪🛒Muebleria Solis🛒🚪 - Los mejores precios estan a tu alcance ✅🎁💰💳</title>
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
