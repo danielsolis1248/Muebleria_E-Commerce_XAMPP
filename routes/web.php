63e40cea1e88011b1e247b3cb23cbf64279a2735
danielsolis1248 <itic18.daniel.solis@iunis.edu.mx>
2022-11-07 19:18:07 -0600

    Ejercicios de rutas de Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,25 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    /*return view('welcome');*/
-    return "Bienvenido a la pagina principal";
+Route::get('/', HomeController::class);
 
-});
+Route::get('cursos', [CursoController::class, 'index']);
 
-Route::get('cursos', function(){
-    return "Bienvenido a la pagina cursos";
-});
+Route::get('cursos/create', [CursoController::class, 'create']);
 
-Route::get('cursos/create', function () {
-    return "En esta pagina podreas crear un curso";
-});
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
 
-/*Route::get('cursos/{curso}', function($curso){
-    return "Bienvenido a la variable de curso: $curso";
-});*/
-
-Route::get('cursos/{curso}/{categortia?}', function ($curso, $categoria = null) {
+/*Route::get('cursos/{curso}/{categortia?}', function ($curso, $categoria = null) {
     if ($categoria) {
         return "Bienvenido al curso $curso, de la categoria $categoria";
     } else {
@@ -39,5 +30,5 @@ Route::get('cursos/{curso}/{categortia?}', function ($curso, $categoria = null) 
     }
 
     
-});
+});*/
 
